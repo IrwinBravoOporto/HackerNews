a2147fbb1abc61286539f98d559e740543c7f14e
irwin <irwinbravooporto@gmail.com>
2021-01-03 11:56:49 -0500

    Initial Commit

---
+++ HackerNews/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  HackerNews
+//
+//  Created by everis on 3/01/21.
+//  Copyright © 2021 Irwin Oporto. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
